f6c95cc30c559c543e27523a1f7d815c0e308747
William Kumler <wkumler@uw.edu>
2023-02-10 16:32:39 -0800

    Adding msdial script

--- msdial_prep_script.R
+++ msdial_prep_script.R
@@ -1,9 +1,19 @@
 
+msdial_dir <- "msdial"
+
+if(dir.exists(msdial_dir)){
+  unlink(msdial_dir, recursive = TRUE, force = TRUE)
+} else {
+  dir.create(msdial_dir)
+}
+file.copy(from = "mzMLs", to = msdial_dir, recursive = TRUE)
+file.copy("msdialparams.txt", paste0(msdial_dir, "/msdialparams.txt"))
+
 msdial_cmd <- paste(
   r"("C:\Program Files\MSDIAL ver.4.9.221218 Windowsx64\MsdialConsoleApp.exe" lcmsdda)",
-  "-i .\\msdial\\",
-  "-o .\\msdial\\",
-  "-m .\\msdial\\msdialparams.txt",
+  paste0("-i .\\", msdial_dir, "\\"),
+  paste0("-o .\\", msdial_dir, "\\"),
+  paste0("-m .\\", msdial_dir, "\\msdialparams.txt"),
   "-p"
 )
 
