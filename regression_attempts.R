79c5c22fbca56ada9cf07b70a09fc8c881d032da
William Kumler <wkumler@uw.edu>
2023-02-16 14:05:44 -0800

    Stabilizing test/train with set.seed and reorganizing to do viz all in one spot

--- regression_attempts.R
+++ regression_attempts.R
@@ -9,6 +9,7 @@ output_folder <- paste0("made_data_", dataset_version, "/")
 features_extracted <- read_csv(paste0(output_folder, "features_extracted.csv")) %>%
   mutate(sn=ifelse(is.infinite(sn), 0, sn))
 
+set.seed(123)
 traintestlist <- features_extracted %>%
   # select(-med_SNR, -med_cor, -med_missed_scans, -shape_cor, -area_cor) %>%
   filter(feat_class%in%c("Good", "Bad")) %>%
@@ -17,9 +18,42 @@ traintestlist <- features_extracted %>%
   split(rbernoulli(nrow(.), 0.2)) %>%
   setNames(c("train", "test"))
 
-
-
 # Single-(ML)-feature regression ----
+single_model <- glm(feat_class~sd_rt, family = binomial, data=traintestlist$train)
+summary(single_model)
+traintestlist$test %>%
+  cbind(pred_class=predict(single_model, ., type="response")>0.5) %>%
+  with(table(feat_class, pred_class)) %>%
+  # caret::confusionMatrix() %>%
+  print()
+
+
+
+# All-feature regression ----
+full_model <- traintestlist$train %>% 
+  select(-feat_id, -blank_found) %>%
+  glm(formula=feat_class~., family = binomial)
+xcms_model <- 
+
+summary(full_model)
+traintestlist$test %>%
+  cbind(pred_class=predict(full_model, ., type="response")>0.9) %>%
+  with(table(feat_class, pred_class)) %>%
+  # caret::confusionMatrix() %>%
+  print()
+
+
+
+# Stepwise AIC optimization using MASS ----
+all_model_feats <- select(traintestlist$train, -feat_id, -blank_found)
+full_model <- glm(formula=feat_class~., family = binomial, data = all_model_feats)
+step_model <- MASS::stepAIC(full_model, direction = "both")
+
+setdiff(names(all_model_feats), broom::tidy(step_model)$term)
+
+
+
+# Visualization (Single-metric, includes ggplot fit) ----
 pred_feat <- "sd_rt"
 init_gp <- traintestlist$train %>%
   mutate(pred_cut=cut(get(pred_feat), pretty(get(pred_feat), n=15), include.lowest=TRUE)) %>%
@@ -42,32 +76,15 @@ reg_gp <- traintestlist$train %>%
   geom_smooth(method="glm", method.args = list(family=binomial), formula='y~x',
               color="black") +
   theme_bw() +
-  labs(x="Metric values", y="Logistic likelihood", color="Classification")
+  labs(x=pred_feat, y="Logistic likelihood", color="Classification")
 cowplot::plot_grid(bar_gp, filled_gp, reg_gp, ncol = 1)
 ggsave(paste0(pred_feat, ".png"), plot = last_plot(), path = "figures", 
        width = 5, height = 6, units = "in", device = "png")
 
-single_model <- glm(feat_class~get(pred_feat), family = binomial, data=traintestlist$train)
-summary(single_model)
-traintestlist$test %>%
-  cbind(pred_class=predict(single_model, ., type="response")>0.5) %>%
-  with(table(feat_class, pred_class)) %>%
-  # caret::confusionMatrix() %>%
-  print()
-
-
-# All-feature regression ----
+# Visualization (Full model, excludes ggplot fit) ----
 full_model <- traintestlist$train %>% 
   select(-feat_id, -blank_found) %>%
   glm(formula=feat_class~., family = binomial)
-summary(full_model)
-traintestlist$test %>%
-  cbind(pred_class=predict(full_model, ., type="response")>0.5) %>%
-  with(table(feat_class, pred_class)) %>%
-  # caret::confusionMatrix() %>%
-  print()
-
-
 init_gp <- traintestlist$test %>%
   cbind(pred_prob=predict(full_model, ., type="response")) %>%
   mutate(pred_prob=cut(pred_prob, pretty(pred_prob, n=15), include.lowest=TRUE)) %>%
@@ -89,114 +106,3 @@ filled_gp <- init_gp +
 cowplot::plot_grid(bar_gp, filled_gp, ncol = 1)
 ggsave("fullmodel_test.png", plot = last_plot(), path = "figures", 
        width = 8, height = 5, units = "in", device = "png")
-
-
-# Just-best regression
-custom_model <- traintestlist$train %>% 
-  select(feat_class, med_cor, med_SNR) %>%
-  glm(formula=feat_class~., family = binomial)
-summary(custom_model)
-traintestlist$test %>%
-  mutate(pred_class=predict(custom_model, ., type="response")>=0.5) %>%
-  with(table(feat_class, pred_class)) %>%
-  # caret::confusionMatrix() %>%
-  print()
-
-# Just xcms regression
-xcms_model <- traintestlist$train %>%
-  select(mean_mz, sd_ppm, mean_rt, sd_rt, mean_pw, sd_pw,
-         log_mean_height, log_sd_height, sn, f, scale, 
-         lmin, feat_class) %>%
-  glm(formula=feat_class~., family = binomial)
-summary(xcms_model)
-traintestlist$test %>%
-  mutate(pred_class=predict(xcms_model, ., type="response")>=0.5) %>%
-  mutate(pred_class=ifelse(pred_class, "Good", "Bad")) %>%
-  mutate(feat_class=ifelse(feat_class, "Good", "Bad")) %>%
-  with(table(feat_class, pred_class)) %>%
-  # caret::confusionMatrix() %>%
-  print()
-
-init_gp <- traintestlist$test %>%
-  cbind(pred_prob=predict(xcms_model, ., type="response")) %>%
-  mutate(pred_prob=cut(pred_prob, pretty(pred_prob, n=15), include.lowest=TRUE)) %>%
-  mutate(feat_class=factor(feat_class, labels = c("Bad", "Good"), levels=c(FALSE, TRUE))) %>%
-  ggplot(aes(x=pred_prob, fill=feat_class)) +
-  theme_bw() +
-  theme(axis.title.x = element_blank()) +
-  labs(fill="Classification") +
-  scale_x_discrete(drop=FALSE)
-bar_gp <- init_gp + 
-  geom_bar() + 
-  labs(y="Count") + 
-  theme(axis.text.x = element_blank()) +
-  ggtitle("XCMS multiple regression model predictions on test set")
-filled_gp <- init_gp + 
-  geom_bar(position = "fill") + 
-  labs(y="Proportion") +
-  theme(axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5))
-cowplot::plot_grid(bar_gp, filled_gp, ncol = 1)
-ggsave("xcmsmodel_test.png", plot = last_plot(), path = "figures", 
-       width = 8, height = 5, units = "in", device = "png")
-
-
-# How does the full model predict "Meh" and "Stans only") ----
-full_model <- traintestlist$train %>% 
-  select(-feat_id, -blank_found) %>%
-  glm(formula=feat_class~., family = binomial)
-pred_prob_vec <- features_extracted %>%
-  select(-blank_found, -feat_id) %>%
-  predict(object=full_model, type = "response")
-
-features_extracted %>%
-  mutate(pred_prob=cut(pred_prob_vec, pretty(pred_prob_vec, n = 10))) %>%
-  ggplot() +
-  geom_bar(aes(x=pred_prob, fill=feat_class)) +
-  facet_wrap(~feat_class, ncol = 1, scales = "free_y") +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
-
-features_extracted %>%
-  mutate(pred_prob) %>%
-  ggplot() +
-  aes(x=mean_rt, y=mean_mz, color=pred_prob) +
-  geom_point() +
-  scale_color_viridis_c() +
-  labs(x="Retention time", y="Mean m/z", color="Predicted\nprobability")
-
-file_data <- read.csv(paste0(output_folder, "file_data.csv"))
-msnexp_filled <- readRDS(paste0(output_folder, "msnexp_filled.rds"))
-peak_data_long <- msnexp_filled %>%
-  chromPeaks() %>%
-  as.data.frame() %>%
-  rownames_to_column()
-peak_data <- msnexp_filled %>%
-  featureDefinitions() %>%
-  as.data.frame() %>%
-  select(mzmed, rtmed, npeaks, peakidx) %>%
-  rownames_to_column("id") %>%
-  unnest_longer(peakidx) %>%
-  rename_with(~paste0("feat_", .x)) %>%
-  mutate(peak_data=peak_data_long[feat_peakidx,]) %>%
-  unnest_wider(peak_data) %>%
-  mutate(filename=file_data$filename[sample])
-
-stan_ts <- peak_data %>%
-  select(feat_id, filename, into) %>%
-  left_join(file_data) %>%
-  filter(samp_type%in%c("Smp", "Std")) %>%
-  tidyr::complete(feat_id, filename, fill=list(into=0)) %>%
-  select(feat_id, filename, into) %>%
-  left_join(file_data) %>%
-  mutate(test_type=str_extract(filename, "Mix1|Mix2")) %>%
-  mutate(test_type=ifelse(is.na(test_type), "Smp", test_type)) %>%
-  nest(data=-feat_id) %>%
-  # slice(1) %>%
-  # unnest() %>%
-  # print(n=Inf)
-  mutate(pval=map_dbl(data, ~broom::tidy(aov(.x$into~.x$samp_type))$p.value[1])) %>%
-  left_join(features_extracted %>% select(feat_id, feat_class))
-stan_ts %>%
-  mutate(pval=log10(pval)) %>%
-  mutate(pval=cut(pval, pretty(pval, 10), include.lowest = TRUE)) %>%
-  ggplot() +
-  geom_bar(aes(x=pval, fill=feat_class), position = "fill")
