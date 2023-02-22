59a7ac49cb133041239b9e03a00045cbb15150b6
William Kumler <wkumler@uw.edu>
2023-02-21 18:19:20 -0800

    Finished training new dataset, overwrote existing visuals

--- training_tool.R
+++ training_tool.R
@@ -13,7 +13,8 @@ library(RaMS)
 options(pillar.sigfig=7)
 
 # dataset_version <- "FT350"
-dataset_version <- "FT2040"
+# dataset_version <- "FT2040"
+dataset_version <- "MS3000"
 output_folder <- paste0("made_data_", dataset_version, "/")
 
 file_data <- read_csv(paste0(output_folder, "file_data.csv")) %>%
