15ad88204091960bae107f5f9660660d914ed7db
William Kumler <wkumler@uw.edu>
2023-03-07 21:46:13 -0800

    Adding shiny app for quick classification

--- pixelpicking_attempts.R
+++ pixelpicking_attempts.R
@@ -2,6 +2,10 @@
 # Setup ----
 library(tidyverse)
 library(RaMS)
+# dataset_version <- "FT2040"
+dataset_version <- "MS3000"
+# dataset_version <- "CultureData"
+output_folder <- paste0("made_data_", dataset_version, "/")
 classified_feats <- read_csv(paste0(output_folder, "classified_feats.csv")) %>%
   select(feature, feat_class)
 msnexp_filled <- readRDS(paste0(output_folder, "msnexp_filled.rds"))
@@ -13,7 +17,7 @@ feature_centers <- featureDefinitions(msnexp_filled) %>%
 
 # Extract the raw data and coerce to pixel matrix ----
 msdata <- readRDS(paste0(output_folder, "msdata.rds"))
-interp_dt <- feature_centers %>%
+interp_df <- feature_centers %>%
   # slice(300:400) %>%
   pmap_dfr(function(...){
     row_data <- list(...)
@@ -33,9 +37,10 @@ interp_dt <- feature_centers %>%
   }) %>%
   group_by(feature, filename) %>%
   mutate(int=int/max(int))
+interp_dt <- as.data.table(interp_df)
 
 # Perform the PCA and check variance explained ----
-pcaoutput <- interp_dt %>%
+pcaoutput <- interp_df %>%
   group_by(feature, filename) %>%
   mutate(rt=rank(rt)) %>%
   # filter(feature%in%sprintf("FT%04d", 440:450)) %>%
@@ -75,7 +80,7 @@ msdata_gp <- msdata$EIC2[mz%between%pmppm(row_data$mzmed, 5)] %>%
   ggplot() +
   geom_line(aes(x=rt, y=int, group=filename)) +
   ggtitle(row_data$feat_id)
-pixel_gp <- interp_dt %>%
+pixel_gp <- interp_df %>%
   filter(feature==row_data$feat_id) %>%
   mutate(samp_type=str_extract(filename, "Smp|Std|Blk|Poo")) %>%
   ggplot() +
@@ -85,10 +90,45 @@ pixel_gp <- interp_dt %>%
 plot(gridExtra::arrangeGrob(msdata_gp, pixel_gp, layout_matrix = matrix(c(
   1,2,2), ncol = 1)))
 
-# I don't think the below is working?
-hc <- hclust(dist(pcaoutput$rotation[,1:3]))
-hc_colors <- classified_feats %>% 
-  mutate(class_col=hcl.colors(6)[as.numeric(factor(feat_class))+1]) %>%
-  pull(class_col, feature)
-plot(hc, hang=-1)
-points(hc$order, y=numeric(length(hc$order)), col=hc_colors[hc$labels[hc$order]], pch=19)
+# Write a shiny app to choose features ----
+library(shiny)
+library(data.table)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      h3("Circle a group to see an average peak from the group"),
+      numericInput("n_init_groups", label="Number of initial groups",
+                   value = 2, min = 1, max = 10, step = 1),
+      numericInput("n_kmeans_dims", label="How many dimensions to use for k-means?",
+                   value = 3, min = 2, max = 10, step = 1),
+      actionButton("chosen_good", label = "Click to choose selection as Good")
+    ),
+    mainPanel(
+      plotlyOutput(outputId = "plotly_pca"),
+      plotOutput(outputId = "avg_selected_peak")
+    )
+  )
+)
+server <- function(input, output, session){
+  output$plotly_pca <- renderPlotly({
+    gp <- pcaoutput$rotation[,1:input$n_kmeans_dims] %>%
+      as.data.frame() %>%
+      mutate(cluster=factor(kmeans(., centers=input$n_init_groups)$cluster)) %>%
+      rownames_to_column("feature") %>%
+      ggplot(aes(x=PC1, y=PC2, label=feature, color=cluster, key=feature)) +
+      geom_text()
+    ggplotly(gp, source = "plotlypca") %>% layout(dragmode="lasso")
+  })
+  output$avg_selected_peak <- renderPlot({
+    ed <- event_data(source = "plotlypca", event = "plotly_selected")
+    req(ed)
+    plot_dt <- interp_dt[
+      feature%chin%ed$key, .(rt=rank(rt, ties.method = "min"), int), by=feature][
+        , .(int=mean(int), iqr_int=IQR(int)), by=rt]
+    plot_dt$rt <- rank(plot_dt$rt)
+    with(plot_dt, plot(rt, int, type="l", lwd=2, ylim=c(0, 1)))
+    with(plot_dt, lines(rt, int+iqr_int))
+    with(plot_dt, lines(rt, int-iqr_int))
+  })
+}
+shinyApp(ui=ui, server = server, options = c(launch.browser=TRUE))
